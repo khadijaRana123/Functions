921e426c0de2822585ef8c0ad21064b10f3af5ae
khadijaRana123 <ranakhadija879@gmail.com>
2024-10-14 11:27:07 +0500

    spread oper file

---
+++ script.js
@@ -0,0 +1,9 @@
+let group1 = ["Ali", "sara"];
+let group2 = ["simra","iqra"];
+let group3 = ["Alisha","Maryam"] ;         
+let group4 = ["kainat","Areba"];
+let group5 = ["Amna","Maliha"];
+let All_group =[...group1,...group2,...group3,...group4,...group5];
+console.log(All_group);
+
+
